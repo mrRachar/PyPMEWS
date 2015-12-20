4ea5c6dff7eccf87719a074ffc611e46b11724b6
mrRachar <matthewross@campars.net>
2015-12-20 18:13:17 +0000

    New Capture Group File, to solve the final regexing

--- temp/capturegroup.py
+++ temp/capturegroup.py
@@ -1,2 +1,19 @@
-def capturegroup(string :str):
-    pass
+number = 0
+
+def getcaturegroup(string: str):
+    global number
+    string = string.lstrip(' \t')
+    if not ':' in string:
+        return False
+    name = string[:string.index(':')]
+    isarray = name.endswith('[]')
+    if isarray:
+        name = name[:-2]
+    if name == '':
+        number += 1
+        return str(number -1) if not isarray else str(number -1) + '()'
+    if not name[0].isalpha() or not all(letter.isalnum() for letter in name[1:]):
+        return False
+    else:
+        return name + ('' if not isarray else '()')
+    
