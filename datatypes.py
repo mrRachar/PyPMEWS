46f43ff7fe61f499d51e6d9d8db40c050c082fe0
mrRachar <matthewross@campars.net>
2015-12-21 15:03:17 +0000

    Using a lot more operators, instead of methods. Equals was moved into node objects. Comments for almost all of `datatypes.py`, making headway with issue #1

--- datatypes.py
+++ datatypes.py
@@ -1,48 +1,128 @@
 from typeenforcing import typed
 
 class Node:
-    value = ''
-    negate = False
-    branches = []
-    reverse = False
-    parent = None
+    """A part of a tree, which will match letters
+
+    It has a value, and branches of other nodes, and is used as part of a tree,
+    so strings can be matched against the expression, by comparing them to nodes
+    """
+
+    value = ''          #The value of the node
+    negate = False      #Whether the node is or isn't negated
+    branches = []       #The nodes this node is connected to
+    reverse = False     #Whether the branches should be reverse-checked
+    parent = None       #The (main) node this is connected to
+
     @typed
     def __init__(self, value: str='', negate: bool=False, branches=None, reverse: bool=False):
+        """Create a new Node, with the value given
+
+        :param value: str: the value of the node
+        :param negate: bool: whether the node's value should be negatively compared
+        :param branches: list: default branches that this node should use
+        :param reverse:  bool: whether branches should be dealt with in reverse (for greedlessness)
+        """
+        #Assign all the parameters to the instance
         self.value = value
-        self.branches = branches or []
+        self.branches = branches or []  #If no branch list was given, create a new list
         self.negate = negate
         self.reverse = reverse
 
     def __iter__(self):
-        yield from self.branches
+        """Iterate through all the branches that the node has
+
+        Yields from the instances branches list
+        """
+        yield from self.branches    #Return all the branches from the branch list
 
     def add(self, node, reverse=False):
-        assert isinstance(node, Node)
-        node.parent = self
-        if self.reverse ^ reverse: #wow, xor
-            self.branches.insert(0, node)
-        else:
-            self.branches.append(node)
+        """Add a branch to this node
+
+        The node will be added the list of branches. If reverse is specified,
+        then it will be added at the front. If reverse is specified for the
+        instance, then it will be added at the opposite end than normal
+
+        :param node: Node: the Node to add as a branches
+        :param reverse: whether to add it at the opposite end, normally the front
+        """
+        assert isinstance(node, Node), 'Only a node may be added as a branch' #Make sure that we're adding a node as a branch
+        node.parent = self #Make the node's parent this instance
+        if self.reverse ^ reverse:          #If it is in reverse mode, or the instance is, but not both, because they should cancel out,
+            self.branches.insert(0, node)   # add it to the front of the branch list
+        else:                               #If however the was no reverse specified, or in the case of a double reverse,
+            self.branches.append(node)      # append the node as the last branch
 
     def __lshift__(self, node):
-        self.add(node)
-        return node
+        """Add a node as a branch
+
+        This behaves like .add, except provides native syntax, and also returns the
+        node afterwards.
+
+        Commonly used in 'i' form:
+            node <<= branch
+        """
+        self.add(node)  #Add the node as a branch
+        return node     #Return this node (especially for 'i' form, for further use)
 
     def __rshift__(self, node):
-        '''Reverse add the element (to front if not completely reversed)'''
-        self.add(node, reverse=True)
-        return node
+        """Add a node as a branch at the opposite than normal end
+
+        This is the negative form or <<, behaving like .add(reverse=True) except providing
+        native syntax, and also returning the node afterwards
+
+        Commonly used in the 'i' form:
+            node >>= branch
+        """
+        self.add(node, reverse=True)    #Add the node as a branch in reverse
+        return node                     #Return this node (especially for 'i' form, for further use)
 
     @typed
     def __getitem__(self, item: int):
+        """Return the item of the given branch"""
         return self.branches[item]
 
-    def __repr__(self):
+    @typed
+    def __repr__(self) -> str:
+        """Represent the node, in initialisation form"""
+        #Format a string with all the initialisation arguments
         return '{}({!r}, {!r}, {!r})'.format(self.__class__.__name__, self.value, self.negate, self.branches)
+
+    @typed
+    def __eq__(self, letter: str) -> bool:
+        """Sees if the node could be equal to the letter
+
+        Will check if they are directly equal, but also if some other value such as
+        'any' or 'space' or a range is set to the nodes value, will see if the letter
+        satisfies the nodes value
+
+        :param letter: str: the letter to compare
+        :param node: Node: the node whose value to compare
+        """
+        if len(self.value) > 1:         #If the nodes value isn't a simple letter, do further testing
+            if self.value == 'any':                                         #If it can be anything
+                return (self.value != '\n') if not self.negate else False   # check it isn't a newline 'cause I don't think they count
+            elif self.value[1] == '-':                                                      #If it has a line in the middle,
+                result = ord(letter) in range(ord(self.value[0]), ord(self.value[2]) + 1)   # see if the letter is in the ascii range given
+                if self.negate:         #If it is negated,
+                    return not result   # negate the result
+                return result           #Otherwise, don't, just don't
+            elif self.value in ['alphanum', 'alpha', 'space']:  #If it is a special name
+                result = {
+                    'alphanum': lambda x: ord('A') <= ord(x) <= ord('Z') or ord('a') <= ord(x) <= ord('z') or ord('0') <= ord(x) <= ord('9'),
+                    'alpha': lambda x: ord('A') <= ord(x) <= ord('Z') or ord('a') <= ord(x) <= ord('z'),
+                    'space': lambda x: x in (' ', '\t'),
+                }[self.value](letter)   #Get the comparison function to see if the letter satisfies the conditions place of it.
+                if self.negate:         #If you must,
+                    return not result   # negate it
+                return result           #Only if you must, though
+        if self.negate:                     #So it isn't special, see if we still have to negate it
+            return self.value != letter     #Return whether they are not equal
+        return self.value == letter     #No negation, so return if it is just some simple equals thing
 
     @property
     @typed
     def configuration(self) -> dict:
+        """Return the configuration of this node"""
         return {
             'value': self.value,
             'negate': self.negate,
@@ -50,77 +130,116 @@ class Node:
         }
 
     def copy(self):
-        return self.__class__(**self.configuration)
+        """Create a duplicate of this node, using all the configurations of it"""
+        return self.__class__(**self.configuration) #Initialise a new instance, with all the same parameters, and return it
 
 class Join(Node):
+    """An empty node, used to connect branches back together"""
     @typed
     def __init__(self, branches=None):
-        self.branches = branches or []
+        """Create a new Join, with the given branches"""
+        self.branches = branches or [] #Set the instance attribute to the given branch, unless None was, then create a new list of branches
 
     @typed
     def __repr__(self) -> str:
+        """Represent the node, in initialisation form"""
+        #Format a string with all the initialisation arguments
         return '{}({!r})'.format(self.__class__.__name__, self.branches)
 
-class Tree(Join):
+    @typed
+    def __eq__(self, string: str) -> bool:
+        """Joins are always 'equal' to a letter, but the letter should be tested against
+         the next node in the implementation"""
+        return True     #Joins are always equals to a string, as they have no value
 
-    tree = None
-    name = None
+class Tree(Join):
+    """A tree of nodes, starting with one which then has branches
+    
+    A tree can also be treated like a Node, and is a Join, as it itself
+    has no value in that expression level. It is tested, and then the
+    match, if any, should be removed from the string, and the tree treated
+    like any Join thenceforth
+    """
+    tree = None         #The first node of the tree
+    name = None         #The name of the capture group, if any
 
     @typed
     def __init__(self, tree=None, branches=None, negate: bool=False, name=None):
-        self.branches = branches or []
-        self.tree = tree or Join()
+        """Create a new tree, with the given Node (and branches to start), branches
+        negation, and capture group name"""
+        #Assign the parameters to the instance
+        self.branches = branches or []  #If no branches were given, create a new list
+        self.tree = tree or Join()      #Create an empty join to add to later if no node was given to start with
         self.negate = negate
         self.name = name
 
     @typed
     def __repr__(self) -> str:
+        """Display the tree of nodes"""
+        #Return the class name, and the tree of nodes in a string
         return '{}({})'.format(self.__class__.__name__, self.tree)
 
     @property
     @typed
     def configuration(self) -> dict:
-        configuration = super().configuration
-        configuration.pop('value')
-        configuration.update({
-            'name': self.name,
-            'tree': self.tree,
+        """Return the configuration of this node"""
+        configuration = super().configuration   #Start with Node's configuration
+        configuration.pop('value')              #Get rid of the value, trees have no value
+        configuration.update({                  #Add,
+            'name': self.name,                  # the capture group name
+            'tree': self.tree,                  # and the tree itself
         })
-        return configuration
+        return configuration                    #Return the configuration of the instance
 
 class Span:
-    start = 0
-    end = 0
+    """A section of a text, with a start and an end
+
+    Equivalent of slice with more functionality
+    """
+    start = 0   #The start of the span
+    end = 0     #The end of the span
 
     @typed
-    def __init__(self, start: (int, tuple) = 0, end: int = 0):
-        if isinstance(start, tuple):
-            self.fromtuple(start)
+    def __init__(self, start: (int, tuple)=0, end: int=0, length: int=0):
+        """Create a new span, from the start to the end
+
+        If a length is given, it will set the end to that far from the start
+        """
+        if isinstance(start, tuple):    #If we've been given a tuple,
+            self.fromtuple(start)       # use it to built the span
             return
+        #Set the instance attributes with the parameters
         self.start = start
         self.end = end
+        if length:                      #If a length was given,
+            self.end = start + length   # set the end the length from the start
 
     @typed
     def fromtuple(self, tuple_: tuple):
-        self.__init__(*tuple_)
+        """Set the span from a tuple"""
+        self.__init__(*tuple_)  #Unpack the tuple, and use init to set values
 
     def __iter__(self):
-        yield from self.bounds
+        '''Go through each bound, the start and then the end'''
+        yield from self.bounds  #Iterate through the tuple of the bounds (start, end)
 
     @property
     @typed
     def bounds(self) -> tuple:
+        '''Return a tuple of the start point and the end'''
         return self.start, self.end
 
     @bounds.setter
     @typed
     def bounds(self, tuple_: tuple):
-        self.fromtuple(*tuple_)
+        '''Reassign the bounds with a tuple'''
+        self.fromtuple(*tuple_)     #Use the tuple to get new bounds'''
 
     @typed
     def __repr__(self) -> str:
-        return "{classname}{values}".format(classname=self.__class__.__name__, values=self.bounds)
+        return "{classname}{values!r}".format(classname=self.__class__.__name__, values=self.bounds)
 
     @typed
     def __add__(self, change: int):
+        '''Offset the span, at both ends, by a integer'''
         return Span(self.start + change, self.end + change)
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from datatypes import *     #Handles data to do with expressions
 ## TODO # comment and clean
 
 class Match:
-    '''A match of an expression against a string
+    """A match of an expression against a string
 
     This contains the match string of what was found,
     the capture groups collected, and the expression
     the match was found by
 
     If there was no match, an empty one of these will
-    be returned by a Matcher, which is falsy'''
+    be returned by a Matcher, which is falsy"""
 
     _match = None       #Store of the match as a string or None
     groups = {}         #All the captures from the match
@@ -20,7 +20,7 @@ class Match:
 
     @typed
     def __init__(self, match: (str, type(None))=None, string: str='', expr: str='', groups=None):
-        '''A new match of an expression against a string
+        """A new match of an expression against a string
 
         With no arguments, creates an empty Match, which
         is falsy
@@ -29,7 +29,7 @@ class Match:
         :param string: The string which the match found against
         :param expr: The expression matched against the string
         :param groups: All the capture groups collected by the match
-        '''
+        """
         #Assign all the parameters to the instance
         self._match = match
         self.groups = groups or {}  #If no group dictionary was given, default to a new one
@@ -38,52 +38,52 @@ class Match:
 
     @typed
     def __bool__(self) -> bool:
-        '''Return whether a match was made or not
+        """Return whether a match was made or not
 
         Uses inner _match attribute, so not equal to match property
-        '''
+        """
         return bool(self._match is not None)    #Return whether the match is none or not
 
     @property
     @typed
     def match(self) -> str:
-        '''Returns the match string found
+        """Returns the match string found
 
         If no match was made, will return an empty string.
         To differentiate between no match, and an empty match,
         see if the match is bool False
-        '''
+        """
         return self._match or ''
 
     @match.setter
     @typed
     def match(self, value: (str, type(None))):
-        '''Sets the match to the value
+        """Sets the match to the value
 
         Will set the inner match value, so None may be given
 
         :param value: str, type(None): the value to set the match to
-        '''
+        """
         self._match = value #Set the inner match attribute to the parameter given
 
     def __iadd__(self, other):
-        '''Add another match on to this instance
+        """Add another match on to this instance
 
         Will add the remainder of the matches string, and combine the groups found.
         This saves having to do these individually.
 
         :param other: Match: the other match to add to this one
-        '''
+        """
         self.match += other.match   #Add the other match on to this one
         self.update(other.groups)   #Merge the two groups, giving precedence to the other's (newer overrides older)
         return self     #Maintain this object, as it is mutable
 
     @typed
     def __repr__(self) -> str:
-        '''Return a representation of the match
+        """Return a representation of the match
 
         {classname}<{expression}>({match}, {string}, {groups})
-        '''
+        """
         return '{}<{}>({!r}, {!r}, {!r})'.format(self.__class__.__name__,   #The class name (Match)
                                        self.expression,                     #The expression matched against the string
                                        self._match,                         #The match found (or None if none found)
@@ -91,13 +91,13 @@ class Match:
                                        self.groups)                         #The groups captured
 
     def update(self, dictionary):
-        '''Update this matches capture groups with another's
+        """Update this matches capture groups with another's
 
         This will also take into account capture collections, and will
         merge them instead of overriding them
 
         :param dictionary: dict(able): the dictionary to update this matches capture groups by
-        '''
+        """
         dictionary = dictionary if isinstance(dictionary, dict) else dict(dictionary) #Convert the dictionary to a dict if it isn't
         for key, value in dictionary.items():     #Go through each item in the other's dictionary
             if key.endswith('[]'):              #If it is a collection, but hasn't been turned into one yet
@@ -114,53 +114,53 @@ class Match:
 
 
 class Matcher:
-    '''A matcher with a stored expression to match against strings
+    """A matcher with a stored expression to match against strings
 
     This takes an expression, breaks it down into a tree, and then
     matches that against strings given, returning matches
-    '''
+    """
     __expression = None     #The private underlying expression, never to be directly changed so always to be a direct representation of the tree
     tree = None             #The tree matched against strings
     def __init__(self, expr: str):
-        '''Create a new matcher from the expression
+        """Create a new matcher from the expression
 
         Sets the expression to the instance property, which is then
         automatically broken down into a tree by the parser, and
         both are stored
 
         To be used to match against strings
-        '''
+        """
         self.expression = expr  #Set the expression to the property, so it will automatically be parsed and set to the tree
 
     @property
     @typed
     def expression(self) -> str:
-        '''The expression the matcher matches'''
+        """The expression the matcher matches"""
         return self.__expression #Return the private instance expression
 
     @expression.setter
     @typed
     def expression(self, expr: str):
-        '''Set the expression and the tree
+        """Set the expression and the tree
 
         Sets the expression to the instance attribute, and then breaks it
         down into a tree using the parser and stores the tree aswell
 
         :param expr: str: the expression to set and breakdown into the tree
-        '''
+        """
         self.__expression = expr
         self.tree = Parser(expr)
 
     @typed
     def match(self, string: str) -> Match:
-        '''Match the expression to the string exactly, from the start
+        """Match the expression to the string exactly, from the start
 
         Will compare the tree to the string given, returning a Match
         instance, which will be falsy if no match is made. It doesn't
         necessarily have to match to the end of the string
 
         :param string: str: the string to match against the expression
-        '''
+        """
         match = self.compare(string)            #Compare the string to the expression
         match.expression = self.__expression    #Set the match up with the expression,
         match.string = string                   # and the given string
@@ -168,7 +168,7 @@ class Matcher:
 
     @typed
     def search(self, string) -> Match:
-        '''NotImplemented fully'''
+        """NotImplemented fully"""
         for substring in (string[i:] for i in range(len(string))):
             match = self.compare(substring)
             if match:
@@ -179,7 +179,7 @@ class Matcher:
 
     @typed
     def negativecompare(self, string: str, node=None) -> Match:
-        '''Compare the string against the node tree segment,
+        """Compare the string against the node tree segment,
         making sure they cannot match
 
         This will go through and check if there is not match, and then
@@ -189,21 +189,21 @@ class Matcher:
 
         :param string: str: the string to negatively compare against
         :param node: the node to start with
-        '''
+        """
         letter = string[0]              #Set the letter to compare to be the first of the string
         node = node or self.tree.tree   #If no node was given, default to the matcher's main tree node (never used in this method)
 
-        if not isinstance(node, Join) and self.equals(letter, node):    #If it isn't a join and there letter equals the node,
+        if not isinstance(node, Join) and letter == node:    #If it isn't a join and there letter equals the node,
             return Match()                                              # they mustn't not equal, so return there wasn't a match
 
         if not node.branches:                                           #If this is the end of the tree segment
             return Match(letter if not isinstance(node, Join) else '')  # only see if it's a join or not, and return the relevant string in a Match
 
-        for branch in node.branches:                                            #Seeing as it has got branches, go through them,
-            if not isinstance(branch, Join) and self.equals(letter, branch):    # and if it does match, a bad thing,
+        for branch in node:                                                     #Seeing as it has got branches, go through them,
+            if not isinstance(branch, Join) and letter == branch:               # and if it does match, a bad thing,
                 return Match()                                                  # return that they don't not match
 
-        for branch in node.branches:        #Now we know all the branches are valid, no possibility of a match,
+        for branch in node:         #Now we know all the branches are valid, no possibility of a match,
             branchmatch = self.negativecompare(string[int(not isinstance(node, Join)):], branch)    # we can negatively compare this branch
             if not branchmatch:                                             #If it doesn't negatively match,
                 continue                                                    # move on
@@ -216,7 +216,7 @@ class Matcher:
 
     @typed
     def compare(self, string: str, node=None) -> Match:
-        '''Compare the string against the node tree segment, seeing if there
+        """Compare the string against the node tree segment, seeing if there
         is a way they could be equal.
 
         Makes sure this node equals the first letter, and then tries to match
@@ -226,7 +226,7 @@ class Matcher:
 
         :param string: str: the string to compare against
         :param node: Node, type(None): the node to start at
-        '''
+        """
         node = node or self.tree.tree   #If there was no node given, default to the node at the top of the tree
         if string == '':    #If we've run out of string
             #Check to see if the tree is not exhausted (there are more branches, and not just one empty Join at most)
@@ -250,14 +250,14 @@ class Matcher:
                     match.update({node.name: match.match})  # add the captured text to it
                 if not string:      #If that leaves the string empty, return
                     #If there are branches, and there is not just one empty Join
-                    if node.branches and not (len(node.branches) == 1 and type(node.branches[0]) is Join and not node.branches[0].branches):
+                    if node.branches and not (len(node.branches) == 1 and type(node[0]) is Join and not node[0].branches):
                         return Match()  #Then the tree is not yet exhausted, so there wasn't a match
                     return match    #Seeing as the tree is exhausted, we can return the match we have made
                 letter = string[0]                          #Reset the letter to the beginning of the new string
 
-        if self.equals(letter, node):                      #When a match has been made already, or the letter and the node's value are equal
+        if letter == node:                      #When a match has been made already, or the letter and the node's value are equal
             if node.branches:                   #If it has branches,
-                for branch in node.branches:    # go through each branch
+                for branch in node:             # go through each branch
                     #Compare the string following this letter, except that Joins don't have any value so check this letter again if it is a Join
                     branchmatch = self.compare(string[int(not isinstance(node, Join)):], branch)
                     if not branchmatch:     #If not a match was made,
@@ -272,41 +272,6 @@ class Matcher:
         else:               #If it isn't equal and no match was made earlier,
             return Match()  # then we haven't got a match
 
-    @typed
-    def equals(self, letter: str, node: Node) -> bool:
-        '''Sees if the letter could be equal to the node
-
-        Will check if they are directly equal, but also if some other value such as
-        'any' or 'space' or a range is set to the nodes value, will see if the letter
-        satisfies the nodes value
-
-        If it is a join, will automatically return true as Joins (and Trees) have no value
-
-        :param letter: str: the letter to compare
-        :param node: Node: the node whose value to compare
-        '''
-        if isinstance(node, Join):  #If we have a Join
-            return True             # return True as they have no real value
-        if len(node.value) > 1:         #If the nodes value isn't a simple letter, do further testing
-            if node.value == 'any':                                         #If it can be anything
-                return (node.value != '\n') if not node.negate else False   # check it isn't a newline 'cause I don't think they count
-            elif node.value[1] == '-':                                                      #If it has a line in the middle,
-                result = ord(letter) in range(ord(node.value[0]), ord(node.value[2]) + 1)   # see if the letter is in the ascii range given
-                if node.negate:         #If it is negated,
-                    return not result   # negate the result
-                return result           #Otherwise, don't, just don't
-            elif node.value in ['alphanum', 'alpha', 'space']:  #If it is a special name
-                result = {
-                    'alphanum': lambda x: ord('A') <= ord(x) <= ord('Z') or ord('a') <= ord(x) <= ord('z') or ord('0') <= ord(x) <= ord('9'),
-                    'alpha': lambda x: ord('A') <= ord(x) <= ord('Z') or ord('a') <= ord(x) <= ord('z'),
-                    'space': lambda x: x in (' ', '\t'),
-                }[node.value](letter)   #Get the comparision function to see if the letter satisfies the conditions place of it.
-                if node.negate:         #If you must,
-                    return not result   # negate it
-                return result           #Only if you must, though
-        if node.negate:                     #So it isn't special, see if we still have to negate it
-            return node.value != letter     #Return whether they are not equal
-        return node.value == letter     #No negation, so return if it is just some simple equals thing
 
 ## TESTS ##
 
